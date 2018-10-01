5e1e17a1b0fdb44f9e971512cc4c5a85a2bde308
ShaneMarusczak <marusczaks@gmail.com>
2018-10-01 15:20:12 -0400

    adding in content

--- src/main/java/org/wecancodeit/reviewsite/ReviewSiteRepository.java
+++ src/main/java/org/wecancodeit/reviewsite/ReviewSiteRepository.java
@@ -24,9 +24,12 @@ public class ReviewSiteRepository {
 		Review htmlcss = new Review(3L, "HTML & CSS", "This page reviews what HTML and CSS are.", "html.jpg",
 				"HTML (the Hypertext Markup Language) and CSS (Cascading Style Sheets) are two of the core technologies for building Web pages. HTML provides the structure of the page, CSS the (visual and aural) layout, for a variety of devices.");
 
+		Review forloops = new Review(4L, "For Loops in Java", "This page explains what a for loop is and how they are useful","forloop.png","For loops come in two different varieties in java. The standard for loop and the enhanced for loop. Both of which iterate through a set of data. The standard loop uses an initialized integer variable, a range and an increment to produce a loop. The enhanced for loop uses objects in a collection to iterate through the collection.");
+			
 		reviewList.put(springMVC.getId(), springMVC);
 		reviewList.put(thymeleaf.getId(), thymeleaf);
 		reviewList.put(htmlcss.getId(), htmlcss);
+		reviewList.put(forloops.getId(), forloops);
 
 	}
 
